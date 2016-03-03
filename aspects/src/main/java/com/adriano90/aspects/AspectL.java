5f6e2e597fe6f5dcf22fb658b0a8b85f060d1677
Adriano90 <elamodeltinto@hotmail.com>
2016-03-03 20:18:34 +0100

    Initial commit

---
+++ aspects/src/main/java/com/adriano90/aspects/AspectL.java
@@ -0,0 +1,25 @@
+package com.adriano90.aspects;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+
+@Aspect
+public class AspectL {
+
+    @Pointcut("execution(* main(..))")
+    public void defineEntryPoint() {
+    }
+
+    @Before("defineEntryPoint()")
+    public void aaa(JoinPoint joinPoint) {
+        System.out.println("aspect before");
+    }
+
+    @After("defineEntryPoint()")
+    public void bbb(JoinPoint joinPoint) {
+        System.out.println("aspect after");
+    }
+}
---
+++ service/src/main/java/com/adriano90/services/App.java
@@ -0,0 +1,16 @@
+package com.adriano90.services;
+
+/**
+ * Created by JuanJuan on 03/03/2016.
+ */
+public class App {
+
+    public void hello(String name) {
+    }
+
+    public static void main(String[] args) {
+        App app = new App();
+        app.hello("world");
+    }
+
+}
